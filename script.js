cbb4ce86825d73a41d7b13f259026abf9e6a5e61
tauheed0110 <tauheed0110@gmail.com>
2024-11-06 09:09:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+
 function getFormvalue() {
     //Write your code here
+    alert(`${fname.value.trim()} ${lname.value.trim()}`)
 
+
+}
+
+window.onload = ()=>{
+    document.getElementById("form1").onsubmit = (e)=>{
+        e.preventDefault();
+        getFormvalue();
+    }
 }
